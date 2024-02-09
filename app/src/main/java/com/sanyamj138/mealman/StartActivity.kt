1e14d61ce9cbf089ab580c4ae373d8adaba1c6aa
Sanyam Jain <sanyamj138@gmail.com>
2024-02-09 23:41:14 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/sanyamj138/mealman/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.sanyamj138.mealman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
