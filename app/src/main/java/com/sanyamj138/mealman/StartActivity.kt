fbf2cf7bbd764991f1e4c7ba2a1e72582452b0e2
Sanyam Jain <sanyamj138@gmail.com>
2024-02-10 22:59:53 +0530

    Login page added

--- app/src/main/java/com/sanyamj138/mealman/StartActivity.kt
+++ app/src/main/java/com/sanyamj138/mealman/StartActivity.kt
@@ -1,11 +1,21 @@
 package com.sanyamj138.mealman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanyamj138.mealman.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+        binding.nxtBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
