79a1bfa74cda1fbda433e44a46cfcd5fd3a19c2d
Sanyam Jain <sanyamj138@gmail.com>
2024-02-10 23:59:27 +0530

    Login and Signup Activity

--- app/src/main/java/com/sanyamj138/mealman/LoginActivity.kt
+++ app/src/main/java/com/sanyamj138/mealman/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.sanyamj138.mealman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.sanyamj138.mealman.databinding.ActivityLoginBinding
+import com.sanyamj138.mealman.databinding.ActivityStartBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding:ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginTxt.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/sanyamj138/mealman/SignUpActivity.kt
+++ app/src/main/java/com/sanyamj138/mealman/SignUpActivity.kt
@@ -1,11 +1,29 @@
 package com.sanyamj138.mealman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanyamj138.mealman.databinding.ActivityLoginBinding
+import com.sanyamj138.mealman.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val binding:ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+        binding.registerBtn.setOnClickListener {
+            intent = Intent(this, ActivityLoginBinding::class.java)
+            startActivity(intent)
+        }
+
+        binding.registerTxt.setOnClickListener {
+            intent = Intent(this, ActivityLoginBinding::class.java)
+            startActivity(intent)
+        }
     }
 }
