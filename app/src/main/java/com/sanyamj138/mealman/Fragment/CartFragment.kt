0661008085ae3c72a9b6d0d13d477f7f24430de6
Sanyam Jain <sanyamj138@gmail.com>
2024-04-04 12:35:30 +0530

    Cart Items Added

--- app/src/main/java/com/sanyamj138/mealman/Fragment/CartFragment.kt
+++ app/src/main/java/com/sanyamj138/mealman/Fragment/CartFragment.kt
@@ -5,56 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sanyamj138.mealman.R
+import com.sanyamj138.mealman.adaptar.CartAdapter
+import com.sanyamj138.mealman.databinding.FragmentCartBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var binding: FragmentCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+
+        val cartFoodName = listOf("Pickle", "Khakara", "Thepla")
+        val cartItemPrice = listOf("296", "239", "152")
+        val cartImage = listOf(R.drawable.pickle, R.drawable.kakhra, R.drawable.thepla)
+        val adapter = CartAdapter(ArrayList(cartFoodName), ArrayList(cartItemPrice), ArrayList(cartImage))
+
+        binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.cartRecyclerView.adapter = adapter
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
---
+++ app/src/main/java/com/sanyamj138/mealman/adaptar/CartAdapter.kt
@@ -0,0 +1,35 @@
+package com.sanyamj138.mealman.adaptar
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sanyamj138.mealman.databinding.CartItemBinding
+
+class CartAdapter(private val cartItems: MutableList<String>, private val CartItemPrice: MutableList<String>, private var CartImage: MutableList<Int>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+
+    private val itemQuantities = IntArray(cartItems.size){1}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CartViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = cartItems.size
+
+    inner class CartViewHolder(private val binding: CartItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+                val quantity = itemQuantities[position]
+                cartFoodName.text = cartItems[position]
+                cartItemPrice.text = CartItemPrice[position]
+                cartImage.setImageResource(CartImage[position])
+                cartItemQuantity.text = quantity.toString()
+            }
+        }
+
+    }
+}
