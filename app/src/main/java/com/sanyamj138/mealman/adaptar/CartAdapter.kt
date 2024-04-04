dd27df6d40e1ad7a8cb4b940c2c70c16f2c355fc
Sanyam Jain <sanyamj138@gmail.com>
2024-04-04 15:58:20 +0530

    Cart Fragment Completed

--- app/src/main/java/com/sanyamj138/mealman/adaptar/CartAdapter.kt
+++ app/src/main/java/com/sanyamj138/mealman/adaptar/CartAdapter.kt
@@ -5,9 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.sanyamj138.mealman.databinding.CartItemBinding
 
-class CartAdapter(private val cartItems: MutableList<String>, private val CartItemPrice: MutableList<String>, private var CartImage: MutableList<Int>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+class CartAdapter(private val CartItems: MutableList<String>, private val CartItemPrice: MutableList<String>, private var CartImage: MutableList<Int>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
 
-    private val itemQuantities = IntArray(cartItems.size){1}
+    private val itemQuantities = IntArray(CartItems.size){1}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
         val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,17 +18,51 @@ class CartAdapter(private val cartItems: MutableList<String>, private val CartIt
         holder.bind(position)
     }
 
-    override fun getItemCount(): Int = cartItems.size
+    override fun getItemCount(): Int = CartItems.size
 
     inner class CartViewHolder(private val binding: CartItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemQuantities[position]
-                cartFoodName.text = cartItems[position]
+                cartFoodName.text = CartItems[position]
                 cartItemPrice.text = CartItemPrice[position]
                 cartImage.setImageResource(CartImage[position])
                 cartItemQuantity.text = quantity.toString()
+
+
+                minusButton.setOnClickListener {
+                    decreaseQuantity(position)
+                }
+                plusButton.setOnClickListener {
+                    increaseQuantity(position)
+                }
+                deleteButton.setOnClickListener {
+                    val itemPosition = adapterPosition
+                    if(itemPosition != RecyclerView.NO_POSITION) {
+                        deleteItem(itemPosition)
+                    }
+                }
             }
+        }
+
+        private fun decreaseQuantity(position: Int) {
+            if(itemQuantities[position] > 1) {
+                itemQuantities[position]--
+                binding.cartItemQuantity.text = itemQuantities[position].toString()
+            }
+        }
+        private fun increaseQuantity(position: Int) {
+            if(itemQuantities[position] < 10) {
+                itemQuantities[position]++
+                binding.cartItemQuantity.text = itemQuantities[position].toString()
+            }
+        }
+        private fun deleteItem(position: Int) {
+            CartItems.removeAt(position)
+            CartImage.removeAt(position)
+            CartItemPrice.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, CartItems.size)
         }
 
     }
