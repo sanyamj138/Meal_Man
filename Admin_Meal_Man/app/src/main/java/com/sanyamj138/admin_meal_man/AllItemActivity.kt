c39f63ee20063108ebae45f9da71732101f622c0
Sanyam Jain <sanyamj138@gmail.com>
2024-04-09 12:27:05 +0530

    All Item UI Added

---
+++ Admin_Meal_Man/app/src/main/java/com/sanyamj138/admin_meal_man/AllItemActivity.kt
@@ -0,0 +1,11 @@
+package com.sanyamj138.admin_meal_man
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_item)
+    }
+}
